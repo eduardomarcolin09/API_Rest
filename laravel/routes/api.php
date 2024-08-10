1360f939598ae63d47d502ac2974eb36f2e8f6bf
Eduardo Marcolin <eduardomarcolin09ntj@gmail.com>
2024-08-10 18:28:22 -0300

    Models e Migrations configuradas

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('users', UserController::class);
+
+// // Rota para listar todos os usuários (index)
+// Route::get('users', [UserController::class, 'index']);
+
+// // Rota para criar um novo usuário (store)
+// Route::post('users', [UserController::class, 'store']);
+
+// // Rota para exibir um usuário específico (show)
+// Route::get('users/{user}', [UserController::class, 'show']);
+
+// // Rota para atualizar um usuário específico (update)
+// Route::put('users/{user}', [UserController::class, 'update']);
+
+// // Rota para excluir um usuário específico (destroy)
+// Route::delete('users/{user}', [UserController::class, 'destroy']);
